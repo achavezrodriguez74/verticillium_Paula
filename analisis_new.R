1400ae17edd1f83b691b875d7cab06adf4b50256
Luciana-cloud <lucianagoku@hotmail.com>
2025-07-01 23:46:59 +0200

    Analysis of all the PCR results with all the genes following the ConoverTest.

--- analisis_new.R
+++ analisis_new.R
@@ -12,7 +12,8 @@ library(ggsignif)
 library(rstatix)  # https://github.com/kassambara/rstatix
 library(tibble)
 library(dplyr)
-
+# Conover-Iman
+library(DescTools)
 
 # LLM1----
 
@@ -209,71 +210,7 @@ png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Pro
 print(figure_spores)
 dev.off()
 
-# Real time PCR ----
-# Not normally and not homogenous variances 
-
-realtimePCR = read.delim("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/datasets/realtimePCR.txt")
-
-model    = aov((Values) ~ Treatments, data = realtimePCR)
-
-# Assumptions----
-# 2. Normality
-ols_plot_resid_qq(model)
-e = resid(model)
-shapiro.test(e)
-# 1. Homogeneity of variances
-ols_plot_resid_fit(model)
-bartlett.test(Values ~ Treatments, data = realtimePCR)
-
-# Kruskall
-kruskal.test(Values ~ Treatments, data = realtimePCR)
-
-# Pairwise
-
-# Dunn's test
-install.packages('dunn.test')
-library(dunn.test)
-dunn.test(realtimePCR$Values, realtimePCR$Treatments, kw=TRUE,
-          method="bh")
-# Conover-Iman
-library(DescTools)
-ConoverTest(realtimePCR$Values, realtimePCR$Treatments,method="holm")
-kwAllPairsConoverTest(realtimePCR$Values ~ as.factor(realtimePCR$Treatments))
-
-# Dwass-steel-critchlow-fligner
-install.packages("PMCMRplus")
-library(PMCMRplus)
-dscfAllPairsTest(realtimePCR$Values ~ as.factor(realtimePCR$Treatments))
-
-# Plotting
-
-stat.test = ConoverTest(realtimePCR$Values, realtimePCR$Treatments,method="holm")
-stat.test = as.data.frame(stat.test[1])
-stat.test$Treatments = row.names(stat.test)
-stat.test = stat.test[, c("Treatments", "mean.rank.diff", "pval")]
-stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 0.01 ~ "*",
-                                                          pval <= 0.01 & pval > 0.001 ~ "**",
-                                                          pval <= 0.001 & pval > 0.0001 ~ "***",
-                                                          pval <= 0.0001 & pval > 0.00001 ~ "****",
-                                                          pval > 0.05 ~ "NS"))
-stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
-group1    = c("Vel1","Vel2","WT","WT")
-group2    = c("IDD","IDD","Vel1","Vel2")
-stat.test = cbind(stat.test,group1,group2)
-
-figure_pcr = ggbarplot(realtimePCR, x = "Treatments", y = "Values", 
-                          ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") + 
-  stat_pvalue_manual((stat.test), label = "p.adj.signif",
-                     y.position = c(32, 26, 26, 29))
-
-figure_pcr
-
-png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.png",
-    width=3500*1.35,height=1969*1.35,res=300)
-print(figure_pcr)
-dev.off()
-
-# Real time PCR FINAL ----
+# Real time PCR LLM1 ----
 
 realtimePCR     = read.delim("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/datasets/realtimePCR.txt")
 
@@ -316,16 +253,9 @@ shapiro.test(e)
 ols_plot_resid_fit(model.PLATE)
 bartlett.test(Values ~ Treatments, data = realtimePCR.PLATE)
 
-# Conover-Iman ----
-library(DescTools)
-
-# SXM ----
-SXM.pairwise = ConoverTest(realtimePCR.SXM$Values, realtimePCR.SXM$Treatments,method="holm")
-SXM.pairwise
-
-library(dunn.test)
-dunn.test(realtimePCR.SXM$Values, realtimePCR.SXM$Treatments, kw=TRUE,
-          method="none")
+#library(dunn.test)
+#dunn.test(realtimePCR.SXM$Values, realtimePCR.SXM$Treatments, kw=TRUE,
+#          method="none")
 
 # Plotting
 
@@ -338,29 +268,30 @@ stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 
                                                           pval <= 0.001 & pval > 0.0001 ~ "***",
                                                           pval <= 0.0001 & pval > 0.00001 ~ "****",
                                                           pval > 0.05 ~ "NS"))
-stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
-group1    = c("Vel2","WT","Vel2","WT","WT")
-group2    = c("IDD","IDD","Vel1","Vel1","Vel2")
+# stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
+group1    = c("Vel1","Vel2","WT","Vel2","WT","WT")
+group2    = c("IDD","IDD","IDD","Vel1","Vel1","Vel2")
 stat.test.SXM = cbind(stat.test,group1,group2)
+stat.test.SXM = stat.test.SXM %>% replace(is.na(.), "***")
 
 figure_pcr.SXM = ggbarplot(realtimePCR.SXM, x = "Treatments", y = "Values", 
                        ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") + 
   stat_pvalue_manual((stat.test.SXM), label = "p.adj.signif",
-                     y.position = c(10, 18, 22, 26,30))
+                     y.position = c(18, 23, 25, 29, 32, 27))
 
 figure_pcr.SXM
 
-png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.SXM.png",
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.SXM.LLM1.png",
     width=3500*1.35,height=1969*1.35,res=300)
 print(figure_pcr.SXM)
 dev.off()
 
 # PDM ----
-PDM.pairwise = ConoverTest(realtimePCR.PDM$Values, realtimePCR.PDM$Treatments,method="holm")
-PDM.pairwise
+#PDM.pairwise = ConoverTest(realtimePCR.PDM$Values, realtimePCR.PDM$Treatments,method="holm")
+#PDM.pairwise
 
-dunn.test(realtimePCR.PDM$Values, realtimePCR.PDM$Treatments, kw=TRUE,
-          method="none")
+#dunn.test(realtimePCR.PDM$Values, realtimePCR.PDM$Treatments, kw=TRUE,
+#          method="none")
 
 # Plotting
 
@@ -385,17 +316,17 @@ figure_pcr.PDM = ggbarplot(realtimePCR.PDM, x = "Treatments", y = "Values",
 
 figure_pcr.PDM
 
-png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.PDM.png",
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.PDM.LLM1.png",
     width=3500*1.35,height=1969*1.35,res=300)
 print(figure_pcr.PDM)
 dev.off()
 
 # PLATE ----
-PLATE.pairwise = ConoverTest(realtimePCR.PLATE$Values, realtimePCR.PLATE$Treatments,method="holm")
-PLATE.pairwise
+#PLATE.pairwise = ConoverTest(realtimePCR.PLATE$Values, realtimePCR.PLATE$Treatments,method="holm")
+#PLATE.pairwise
 
-dunn.test(realtimePCR.PLATE$Values, realtimePCR.PLATE$Treatments, kw=TRUE,
-          method="none",list=TRUE)
+#dunn.test(realtimePCR.PLATE$Values, realtimePCR.PLATE$Treatments, kw=TRUE,
+#          method="none",list=TRUE)
 
 # Plotting
 
@@ -416,11 +347,11 @@ stat.test.PLATE = cbind(stat.test,group1,group2)
 figure_pcr.PLATE = ggbarplot(realtimePCR.PLATE, x = "Treatments", y = "Values", 
                        ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") + 
   stat_pvalue_manual((stat.test.PLATE), label = "p.adj.signif",
-                     y.position = c(32, 26, 26, 29))
+                     y.position = c(26, 12, 29, 32))
 
 figure_pcr.PLATE
 
-png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.PLATE.png",
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.PLATE.LLM1.png",
     width=3500*1.35,height=1969*1.35,res=300)
 print(figure_pcr.PLATE)
 dev.off()
@@ -468,5 +399,261 @@ png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Pro
 print(figure_plant_AML)
 dev.off()
 
+# Real time PCR AML1 ----
+# Not normally and not homogenous variances
+
+realtimePCR = read.delim("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/datasets/realtimePCR_AML1.txt")
+
+# SXM (not normal not homogeneous variances) ----
+realtimePCR.SXM   = realtimePCR %>% filter(Medium == "SXM")
+model.SXM         = aov((Values) ~ Treatments, data = realtimePCR.SXM)
+
+# Assumptions
+# 2. Normality
+ols_plot_resid_qq(model.SXM)
+e = resid(model.SXM)
+shapiro.test(e)
+# 1. Homogeneity of variances
+ols_plot_resid_fit(model.SXM)
+bartlett.test((Values) ~ Treatments, data = realtimePCR.SXM)
+
+# Plotting
+
+stat.test = ConoverTest(realtimePCR.SXM$Values, realtimePCR.SXM$Treatments,method="holm")
+stat.test = as.data.frame(stat.test[1])
+stat.test$Treatments = row.names(stat.test)
+stat.test = stat.test[, c("Treatments", "mean.rank.diff", "pval")]
+stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 0.01 ~ "*",
+                                                          pval <= 0.01 & pval > 0.001 ~ "**",
+                                                          pval <= 0.001 & pval > 0.0001 ~ "***",
+                                                          pval <= 0.0001 & pval > 0.00001 ~ "****",
+                                                          pval > 0.05 ~ "NS"))
+#stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
+#group1    = c("Vel2","WT","Vel2","WT","WT")
+#group2    = c("IDD","IDD","Vel1","Vel1","Vel2")
+#stat.test.SXM = cbind(stat.test,group1,group2)
+stat.test.SXM = stat.test
+
+figure_pcr.SXM.AML1 = ggbarplot(realtimePCR.SXM, x = "Treatments", y = "Values", 
+                                ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") # + 
+#  stat_pvalue_manual((stat.test.SXM), label = "p.adj.signif",
+#                     y.position = c(10, 18, 22, 26,30))
+
+figure_pcr.SXM.AML1
+
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.SXM.AML1.png",
+    width=3500*1.35,height=1969*1.35,res=300)
+print(figure_pcr.SXM.AML1)
+dev.off()
+
+# PDM (normal not homogeneous variances) ----
+realtimePCR.PDM   = realtimePCR %>% filter(Medium == "PDM")
+model.PDM         = aov((Values) ~ Treatments, data = realtimePCR.PDM)
+
+# Assumptions
+# 2. Normality
+ols_plot_resid_qq(model.PDM)
+e = resid(model.PDM)
+shapiro.test(e)
+# 1. Homogeneity of variances
+ols_plot_resid_fit(model.PDM)
+bartlett.test(Values ~ Treatments, data = realtimePCR.PDM)
+
+# Plotting
+
+stat.test = ConoverTest(realtimePCR.PDM$Values, realtimePCR.PDM$Treatments,method="holm")
+stat.test = as.data.frame(stat.test[1])
+stat.test$Treatments = row.names(stat.test)
+stat.test = stat.test[, c("Treatments", "mean.rank.diff", "pval")]
+stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 0.01 ~ "*",
+                                                          pval <= 0.01 & pval > 0.001 ~ "**",
+                                                          pval <= 0.001 & pval > 0.0001 ~ "***",
+                                                          pval <= 0.0001 & pval > 0.00001 ~ "****",
+                                                          pval > 0.05 ~ "NS"))
+stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
+group1    = c("ΔVel1","ΔVel2","ΔVel1","ΔVel2")
+group2    = c("IDD","IDD","WT","WT")
+stat.test.PDM = cbind(stat.test,group1,group2)
+
+figure_pcr.PDM.AML1 = ggbarplot(realtimePCR.PDM, x = "Treatments", y = "Values", 
+                                ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") + 
+  stat_pvalue_manual((stat.test.PDM), label = "p.adj.signif",
+                     y.position = c(12, 18, 15, 8))
+
+figure_pcr.PDM.AML1
+
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.PDM.AML1.png",
+    width=3500*1.35,height=1969*1.35,res=300)
+print(figure_pcr.PDM.AML1)
+dev.off()
+
+# PLATE (normal not homogeneous variances) ----
+realtimePCR.PLATE = realtimePCR %>% filter(Medium == "PLATE")
+model.PLATE       = aov((Values) ~ Treatments, data = realtimePCR.PLATE)
+
+# Assumptions
+# 2. Normality
+ols_plot_resid_qq(model.PLATE)
+e = resid(model.PLATE)
+shapiro.test(e)
+# 1. Homogeneity of variances
+ols_plot_resid_fit(model.PLATE)
+bartlett.test(Values ~ Treatments, data = realtimePCR.PLATE)
+
+# Plotting
+
+stat.test = ConoverTest(realtimePCR.PLATE$Values, realtimePCR.PLATE$Treatments,method="holm")
+stat.test = as.data.frame(stat.test[1])
+stat.test$Treatments = row.names(stat.test)
+stat.test = stat.test[, c("Treatments", "mean.rank.diff", "pval")]
+stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 0.01 ~ "*",
+                                                          pval <= 0.01 & pval > 0.001 ~ "**",
+                                                          pval <= 0.001 & pval > 0.0001 ~ "***",
+                                                          pval <= 0.0001 & pval > 0.00001 ~ "****",
+                                                          pval > 0.05 ~ "NS"))
+stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
+group1    = c("ΔVel2")
+group2    = c("WT")
+stat.test.PLATE = cbind(stat.test,group1,group2)
+
+figure_pcr.PLATE.AML1 = ggbarplot(realtimePCR.PLATE, x = "Treatments", y = "Values", 
+                                  ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") + 
+  stat_pvalue_manual((stat.test.PLATE), label = "p.adj.signif",
+                     y.position = c(1.5))
+
+figure_pcr.PLATE.AML1
+
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.PLATE.AML1.png",
+    width=3500*1.35,height=1969*1.35,res=300)
+print(figure_pcr.PLATE.AML1)
+dev.off()
+
+# Real time PCR NML1 ----
+# Not normally and not homogenous variances
+
+realtimePCR = read.delim("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/datasets/realtimePCR_NML1.txt")
+
+# SXM (not normal not homogeneous variances) ----
+realtimePCR.SXM   = realtimePCR %>% filter(Medium == "SXM")
+model.SXM         = aov((Values) ~ Treatments, data = realtimePCR.SXM)
+
+# Assumptions
+# 2. Normality
+ols_plot_resid_qq(model.SXM)
+e = resid(model.SXM)
+shapiro.test(e)
+# 1. Homogeneity of variances
+ols_plot_resid_fit(model.SXM)
+bartlett.test((Values) ~ Treatments, data = realtimePCR.SXM)
+
+# Plotting
+
+stat.test = ConoverTest(realtimePCR.SXM$Values, realtimePCR.SXM$Treatments,method="holm")
+stat.test = as.data.frame(stat.test[1])
+stat.test$Treatments = row.names(stat.test)
+stat.test = stat.test[, c("Treatments", "mean.rank.diff", "pval")]
+stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 0.01 ~ "*",
+                                                          pval <= 0.01 & pval > 0.001 ~ "**",
+                                                          pval <= 0.001 & pval > 0.0001 ~ "***",
+                                                          pval <= 0.0001 & pval > 0.00001 ~ "****",
+                                                          pval > 0.05 ~ "NS"))
+stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
+group1    = c("ΔVel1","ΔVel2","ΔVel1","ΔVel2")
+group2    = c("IDD","IDD","WT","WT")
+stat.test.SXM = cbind(stat.test,group1,group2)
+
+figure_pcr.SXM.NML1 = ggbarplot(realtimePCR.SXM, x = "Treatments", y = "Values", 
+                                ylab = "Relative Normalized Expression", xlab = "", add = "mean_se")  + 
+  stat_pvalue_manual((stat.test.SXM), label = "p.adj.signif",
+                     y.position = c(4, 6, 8, 4))
+
+figure_pcr.SXM.NML1
+
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.SXM.NML1.png",
+    width=3500*1.35,height=1969*1.35,res=300)
+print(figure_pcr.SXM.NML1)
+dev.off()
+
+# PDM (normal not homogeneous variances) ----
+realtimePCR.PDM   = realtimePCR %>% filter(Medium == "PDM")
+model.PDM         = aov((Values) ~ Treatments, data = realtimePCR.PDM)
+
+# Assumptions
+# 2. Normality
+ols_plot_resid_qq(model.PDM)
+e = resid(model.PDM)
+shapiro.test(e)
+# 1. Homogeneity of variances
+ols_plot_resid_fit(model.PDM)
+bartlett.test(Values ~ Treatments, data = realtimePCR.PDM)
+
+# Plotting
+
+stat.test = ConoverTest(realtimePCR.PDM$Values, realtimePCR.PDM$Treatments,method="holm")
+stat.test = as.data.frame(stat.test[1])
+stat.test$Treatments = row.names(stat.test)
+stat.test = stat.test[, c("Treatments", "mean.rank.diff", "pval")]
+stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 0.01 ~ "*",
+                                                          pval <= 0.01 & pval > 0.001 ~ "**",
+                                                          pval <= 0.001 & pval > 0.0001 ~ "***",
+                                                          pval <= 0.0001 & pval > 0.00001 ~ "****",
+                                                          pval > 0.05 ~ "NS"))
+stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
+group1    = c("ΔVel1","ΔVel2")
+group2    = c("IDD","IDD")
+stat.test.PDM = cbind(stat.test,group1,group2)
+
+figure_pcr.PDM.NML1 = ggbarplot(realtimePCR.PDM, x = "Treatments", y = "Values", 
+                                ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") + 
+  stat_pvalue_manual((stat.test.PDM), label = "p.adj.signif",
+                     y.position = c(5, 7))
+
+figure_pcr.PDM.NML1
+
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.PDM.NML1.png",
+    width=3500*1.35,height=1969*1.35,res=300)
+print(figure_pcr.PDM.NML1)
+dev.off()
+
+# PLATE (normal not homogeneous variances) ----
+realtimePCR.PLATE = realtimePCR %>% filter(Medium == "PLATE")
+model.PLATE       = aov((Values) ~ Treatments, data = realtimePCR.PLATE)
+
+# Assumptions
+# 2. Normality
+ols_plot_resid_qq(model.PLATE)
+e = resid(model.PLATE)
+shapiro.test(e)
+# 1. Homogeneity of variances
+ols_plot_resid_fit(model.PLATE)
+bartlett.test(Values ~ Treatments, data = realtimePCR.PLATE)
+
+# Plotting
+
+stat.test = ConoverTest(realtimePCR.PLATE$Values, realtimePCR.PLATE$Treatments,method="holm")
+stat.test = as.data.frame(stat.test[1])
+stat.test$Treatments = row.names(stat.test)
+stat.test = stat.test[, c("Treatments", "mean.rank.diff", "pval")]
+stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 0.01 ~ "*",
+                                                          pval <= 0.01 & pval > 0.001 ~ "**",
+                                                          pval <= 0.001 & pval > 0.0001 ~ "***",
+                                                          pval <= 0.0001 & pval > 0.00001 ~ "****",
+                                                          pval > 0.05 ~ "NS"))
+stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
+group1    = c("ΔVel1")
+group2    = c("IDD")
+stat.test.PLATE = cbind(stat.test,group1,group2)
+
+figure_pcr.PLATE.NML1 = ggbarplot(realtimePCR.PLATE, x = "Treatments", y = "Values", 
+                                  ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") + 
+  stat_pvalue_manual((stat.test.PLATE), label = "p.adj.signif",
+                     y.position = c(4))
+
+figure_pcr.PLATE.NML1
+
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.PLATE.NML1.png",
+    width=3500*1.35,height=1969*1.35,res=300)
+print(figure_pcr.PLATE.NML1)
+dev.off()
 
 
--- old/analisis.R
+++ old/analisis.R
@@ -141,6 +141,79 @@ ggboxplot(plates, x = "Biological.Rep", y = "Normalized",
           ylab = "Normalized melanization", xlab = "") + 
   stat_compare_means(comparisons = my_comparisons, label = "p.signif")
 
+# Real time PCR FINAL ----
+
+# Not normally and not homogenous variances 
+
+realtimePCR = read.delim("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/datasets/realtimePCR.txt")
+
+model    = aov((Values) ~ Treatments, data = realtimePCR)
+
+# Assumptions----
+# 2. Normality
+ols_plot_resid_qq(model)
+e = resid(model)
+shapiro.test(e)
+# 1. Homogeneity of variances
+ols_plot_resid_fit(model)
+bartlett.test(Values ~ Treatments, data = realtimePCR)
+
+# Kruskall
+kruskal.test(Values ~ Treatments, data = realtimePCR)
+
+# Pairwise
+
+# Dunn's test
+install.packages('dunn.test')
+library(dunn.test)
+dunn.test(realtimePCR$Values, realtimePCR$Treatments, kw=TRUE,
+          method="bh")
+# Conover-Iman
+library(DescTools)
+ConoverTest(realtimePCR$Values, realtimePCR$Treatments,method="holm")
+kwAllPairsConoverTest(realtimePCR$Values ~ as.factor(realtimePCR$Treatments))
+
+# Dwass-steel-critchlow-fligner
+install.packages("PMCMRplus")
+library(PMCMRplus)
+dscfAllPairsTest(realtimePCR$Values ~ as.factor(realtimePCR$Treatments))
+
+# Plotting
+
+stat.test = ConoverTest(realtimePCR$Values, realtimePCR$Treatments,method="holm")
+stat.test = as.data.frame(stat.test[1])
+stat.test$Treatments = row.names(stat.test)
+stat.test = stat.test[, c("Treatments", "mean.rank.diff", "pval")]
+stat.test = stat.test %>% mutate(p.adj.signif = case_when(pval <= 0.05 & pval > 0.01 ~ "*",
+                                                          pval <= 0.01 & pval > 0.001 ~ "**",
+                                                          pval <= 0.001 & pval > 0.0001 ~ "***",
+                                                          pval <= 0.0001 & pval > 0.00001 ~ "****",
+                                                          pval > 0.05 ~ "NS"))
+stat.test = stat.test %>% filter(!(p.adj.signif == "NS"))
+group1    = c("Vel1","Vel2","WT","WT")
+group2    = c("IDD","IDD","Vel1","Vel2")
+stat.test = cbind(stat.test,group1,group2)
+
+figure_pcr = ggbarplot(realtimePCR, x = "Treatments", y = "Values", 
+                       ylab = "Relative Normalized Expression", xlab = "", add = "mean_se") + 
+  stat_pvalue_manual((stat.test), label = "p.adj.signif",
+                     y.position = c(32, 26, 26, 29))
+
+figure_pcr
+
+png("C:/Users/lucia/OneDrive - Wageningen University & Research/UCI_projects/Project_8 (Reviews)/Family/Pere/doctorado/Figures/figure_pcr.png",
+    width=3500*1.35,height=1969*1.35,res=300)
+print(figure_pcr)
+dev.off()
 
 
+# Conover-Iman ----
+library(DescTools)
 
+# SXM ----
+SXM.pairwise = ConoverTest(realtimePCR.SXM$Values, realtimePCR.SXM$Treatments,method="holm")
+SXM.pairwise
+
+library(dunn.test)
+dunn.test(realtimePCR.SXM$Values, realtimePCR.SXM$Treatments, kw=TRUE,
+          method="none")
